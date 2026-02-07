1158d9e3549dee5a7a1e8bf1c844b98d258db234
Ramazan Bozkurt <ramazan.bozkurt.dev@gmail.com>
2026-02-07 11:41:20 +0300

    Username and password updates are added. Authentication principal change to email.

--- frontend/texttosql/src/app/components/auth/login/login.component.ts
+++ frontend/texttosql/src/app/components/auth/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent {
   private readonly authService = inject(AuthService);
 
   loginForm: FormGroup = this.fb.group({
-    username: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required]]
   });
 
